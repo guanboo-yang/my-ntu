1145a650e99bb6f0e274953f5ecefd3eb8acffee
Guan-Bo Yang <b09902064@ntu.edu.tw>
2022-09-25 01:28:31 +0800

    [MY NTU] Try Fix Relative Route

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,7 +44,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createVueRouter({
-  history: createWebHistory(),
+  history: createWebHistory(window.location.pathname),
   routes
 })
 
